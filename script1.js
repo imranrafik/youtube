d30bdf99588a89003da0aa3b4cba2b1ac8104fa3
imranrafik <imranmondal9369@gmail.com>
2025-05-02 15:05:46 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+/*============================= typed name  ============================ */
+var typed = new Typed (".typing",{
+    strings:["","Hafez Of The Quran","IBA Of Smart Value","Web Developer","Student Of Fazil"],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+})
